3b23441864f627cd400a4da76b34bc332752f7d6
Bulat <bulat.galimov.80@mail.ru>
2025-04-10 19:44:49 +0300

    Добавил переменные основных цветов шаблона

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!-- src/App.vue -->
 <template>
   <div class="p-6">
-    <nav class="flex gap-4 text-blue-600 underline mb-6">
+    <nav class="flex gap-4 bg-orange-foreground-200  underline mb-6">
       <router-link to="/">Главная</router-link>
       <router-link to="/about">О проекте</router-link>
       <router-link to="/news">Новости</router-link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-        "./index.html",
-        "./src/**/*.{vue,js,ts,jsx,tsx}",
-    ],
+export default {
+    content: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
     theme: {
         extend: {
             colors: {
-                primary: '#4F46E5',
+                orange: {
+                    50: 'var(--color-orange-50)',
+                    100: 'var(--color-orange-100)',
+                    200: 'var(--color-orange-200)',
+                    300: 'var(--color-orange-300)',
+                    400: 'var(--color-orange-400)',
+                    500: 'var(--color-orange-500)',
+                    600: 'var(--color-orange-600)',
+                    700: 'var(--color-orange-700)',
+                    800: 'var(--color-orange-800)',
+                    900: 'var(--color-orange-900)',
+                },
+                'orange-foreground': {
+                    100: 'var(--color-orange-foreground-100)',
+                    200: 'var(--color-orange-foreground-200)',
+                    400: 'var(--color-orange-foreground-400)',
+                    700: 'var(--color-orange-foreground-700)',
+                },
             },
         },
     },
