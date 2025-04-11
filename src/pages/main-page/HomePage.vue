024c55cf0c43e6a42398549a3e8b6d6669a1936f
Bulat <bulat.galimov.80@mail.ru>
2025-04-11 09:45:06 +0300

    Сделал первый блок

--- src/pages/main-page/HomePage.vue
+++ src/pages/main-page/HomePage.vue
@@ -1,8 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
   <div class="home-page">
-    <h1>Мейн</h1>
+    <div class="content-rectangle h-[60px] bg-orange-500"/>
+
+
+    <div class="partners-info bg-white flex justify-center">
+      <div class=" flex flex-col gap-[30px]">
+        <div class="partners-info__title p-[10px] max-w-[1024px]">
+          <h1 class="text-center leading-none text-4xl">
+            Проект создан при поддержке Федерального государственного бюджетного учреждения
+            "Фонд содействия развитию малых форм предприятий в научно-технической сфере в рамках программы "Студенческий стартап"
+          </h1>
+        </div>
+        <div class="partners-info__icons flex justify-between max-w-[1060px]">
+          <div class="partners-info__icon">
+            <img src="@/assets/img/fond-sodeystviya.svg"/>
+          </div>
+          <div class="partners-info__icon">
+            <img src="@/assets/img/kpfu.svg"/>
+          </div>
+        </div>
+      </div>
+    </div>
+    <section class="project-info bg-orange-500 text-white py-8" id="about-project">
+      <div class="project-info__content max-w-[1024px] mx-auto">
+        <h1 class="text-center leading-none text-4xl mb-4">
+          О проекте
+        </h1>
+        <div>
+
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
