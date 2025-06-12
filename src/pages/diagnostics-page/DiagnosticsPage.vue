f0c4907261f146c01624f0c1e56db2b6ba352363
Булат Галимов <b.galimov@bambit.ru>
2025-06-12 19:27:03 +0300

    3 тест

--- src/pages/diagnostics-page/DiagnosticsPage.vue
+++ src/pages/diagnostics-page/DiagnosticsPage.vue
@@ -40,34 +40,38 @@ const psychologyCards = [
 const cardsWithImage = [
   {
     title: "Опросник «Отношение педагога к родителям»",
-    image: "/public/img/infoCard_1.svg"
+    image: "/img/infoCard_1.svg"
   },
   {
     title: "Опросник «Отношение педагога к детям»",
-    image: "/public/img/infoCard_2.svg"
+    image: "/img/infoCard_2.svg"
+  },
+  {
+    title: "«Проверьте развитие ребенка»",
+    image: "/img/infoCard_6.svg"
   },
   {
     title: "«Учебная мотивация школьников»",
-    image: "/public/img/infoCard_3.svg"
+    image: "/img/infoCard_3.svg"
   },
   {
     title: "«Уровень тревожности ребёнка»",
-    image: "/public/img/infoCard_4.svg"
+    image: "/img/infoCard_4.svg"
   },
   {
     title: "«Родительское выгорание»",
-    image: "/public/img/infoCard_5.svg"
+    image: "/img/infoCard_5.svg"
   },
   {
     title: "Диагностика детских страхов (по возрасту)",
-    image: "/public/img/infoCard_6.svg"
+    image: "/img/infoCard_6.svg"
   }
 ];
 
 </script>
 
 <template>
-  <Header />
+  <Header/>
   <section class="diagnostics px-[55px] py-8">
     <div class="flex lg:flex-row flex-col gap-6 items-center">
       <div class="flex flex-col lg:gap-16 gap-6">
@@ -75,16 +79,17 @@ const cardsWithImage = [
           Диагностика как первый шаг к осознанному родительству
         </h1>
         <p class="text-4xl leading-[100%]">
-          Данный раздел посвящен психологическому и педагогическому материалу, включающий в себя адаптированную литературу, а также тестирования.педагогами и психологами.
+          Данный раздел посвящен психологическому и педагогическому материалу, включающий в себя адаптированную
+          литературу, а также тестирования.педагогами и психологами.
         </p>
       </div>
       <img
-        src="/img/diagnostics.svg"
-        alt="diagnostics"
+          src="/img/diagnostics.svg"
+          alt="diagnostics"
       >
     </div>
   </section>
-  <div class="content-rectangle h-[60px] bg-orange-500 mb-[50px]" />
+  <div class="content-rectangle h-[60px] bg-orange-500 mb-[50px]"/>
   <section class="pedagogy px-[55px] py-8">
     <div class="flex flex-col gap-[50px]">
       <div class="flex flex-col items-center text-center gap-6 ">
@@ -98,20 +103,20 @@ const cardsWithImage = [
       </div>
       <div class="flex lg:flex-row flex-col-reverse items-center justify-center gap-[50px] ">
         <img
-          src="/img/pedagogy.svg"
-          alt="pedagogy"
+            src="/img/pedagogy.svg"
+            alt="pedagogy"
         >
         <div class="grid grid-cols-2 gap-[41px] lg:max-w-[535px]">
           <router-link
-            v-for="(card, index) in cards"
-            :key="index"
-            :to="`/diagnostics/pedagogy/${index}`"
-            target="_blank"
+              v-for="(card, index) in cards"
+              :key="index"
+              :to="`/diagnostics/pedagogy/${index}`"
+              target="_blank"
           >
             <InfoCard
-              :title="card.title"
-              :text="card.preview"
-              class="p-3 lg:h-full text-center cursor-pointer transition-transform transform hover:scale-105 hover:shadow-lg"
+                :title="card.title"
+                :text="card.preview"
+                class="p-3 lg:h-full text-center cursor-pointer transition-transform transform hover:scale-105 hover:shadow-lg"
             />
           </router-link>
         </div>
@@ -130,11 +135,11 @@ const cardsWithImage = [
     </div>
     <div class="flex flex-col gap-8">
       <router-link
-        v-for="(card, key) in psychologyCards"
-        :key="key"
-        :to="`/diagnostics/psychology/${key}`"
-        target="_blank"
-        class="bg-white lg:text-2xl text-4xl text-left rounded-[5px] px-[30px] py-[18px] cursor-pointer hover:bg-orange-50 transition"
+          v-for="(card, key) in psychologyCards"
+          :key="key"
+          :to="`/diagnostics/psychology/${key}`"
+          target="_blank"
+          class="bg-white lg:text-2xl text-4xl text-left rounded-[5px] px-[30px] py-[18px] cursor-pointer hover:bg-orange-50 transition"
       >
         <p>{{ card.title }}</p>
       </router-link>
@@ -153,16 +158,16 @@ const cardsWithImage = [
     </div>
     <div class="grid lg:grid-cols-3 grid-cols-2 gap-[30px] px-[72px]">
       <router-link
-        v-for="(card, key) in cardsWithImage"
-        :key="key"
-        :to="`/test/${key+1}`"
-        target="_blank"
-        class="cursor-pointer"
+          v-for="(card, key) in cardsWithImage"
+          :key="key"
+          :to="`/test/${key+1}`"
+          target="_blank"
+          class="cursor-pointer"
       >
         <InfoCard
-          :title="card.title"
-          :image="card.image"
-          class="px-3 py-[26px] text-white text-center"
+            :title="card.title"
+            :image="card.image"
+            class="px-3 py-[26px] text-white text-center"
         />
       </router-link>
     </div>
@@ -178,11 +183,11 @@ const cardsWithImage = [
         шаги.
       </p>
       <Button
-        label="Записаться на консультацию"
-        color="white"
-        size="large"
+          label="Записаться на консультацию"
+          color="white"
+          size="large"
       />
     </div>
   </section>
-  <Footer />
+  <Footer/>
 </template>
