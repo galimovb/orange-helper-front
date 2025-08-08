ae3487969a7f772ab4da6f62a99179a8e380273a
L1llong <longma2003@mail.ru>
2025-08-08 11:55:42 +0700

    добавил эскпресс тест

--- src/pages/test-page/TestPage.vue
+++ src/pages/test-page/TestPage.vue
@@ -5,6 +5,9 @@ import {useTestStore} from '@/stores/testStore';
 
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
+import Button from "@/components/Button.vue";
+import Input from "@/components/Input.vue";
+import {Label} from "radix-vue";
 
 const route = useRoute();
 const testId = route.params.id;
@@ -15,12 +18,8 @@ const currentQuestionIndex = ref(0);
 
 const answers = ref({});
 const result = ref(null);
-const isImageVisible = ref(true); // Флаг для отображения изображения
-
-
-onMounted(async () => {
-  await testStore.fetchTestById(testId);
-});
+const isImageVisible = ref(true);
+const isExpressTest = ref(true);
 
 const prevQuestion = () => {
   if (currentQuestionIndex.value === 0) {
@@ -75,13 +74,87 @@ const showResult = () => {
     scoreBreakdown,
   };
 };
+
+const goToExpressTest = () => {
+  isExpressTest.value = false;
+};
+
+onMounted(async () => {
+  await testStore.fetchTestById(testId);
+});
+
 </script>
 
 <template>
   <Header/>
   <section class="test-page px-8 py-3 md:px-11 md:py-6 lg:px-[55px] lg:py-8">
+
+    <section v-if="testStore.currentTest?.isExpressTest && isExpressTest">
+      <div class="flex items-center justify-between mt-4
+
+      lg:mt-9">
+        <div class="flex flex-col gap-9">
+          <div class="text-2xl md:text-3xl lg:text-5xl leading-[100%]">
+            <label class="block text-orange-500 mb-4">
+              Экспресс-тестирование
+            </label>
+            <div>
+              {{ testStore.currentTest?.title }}
+            </div>
+          </div>
+          <div class="flex flex-col gap-3 md:gap-5 lg:gap-9">
+            <h5 class="text-base md:text-2xl lg:text-3xl leading-[100%]">
+              Ваш E-mail
+            </h5>
+            <form
+                @submit.prevent="subscribeToNewsletter"
+                class="flex flex-col gap-5 items-left max-w-[742px]"
+            >
+              <div>
+                <Input
+                    v-model="email"
+                    type="email"
+                    required
+                    placeholder="person123@gmail.com"
+                />
+              </div>
+              <div class="flex items-center gap-2">
+                <Input
+                    v-model="consent"
+                    type="checkbox"
+                    id="consent"
+                    name="consent"
+                    class="!w-4 !h-4 md:!w-6 md:!h-6 lg:!w-9 lg:!h-9"
+                    required
+                />
+                <label for="consent" class="text-xs md:text-sm lg:text-2xl leading-[100%] text-gray-600">
+                  Я ознакомлен(-а) с Политикой конфиденциальности
+                </label>
+              </div>
+              <label class="text-xs md:text-sm lg:text-2xl leading-[100%]">
+                Результаты теста будут отправлены на электронную почту. Для этого заполните поле
+              </label>
+              <button
+                  @click="goToExpressTest"
+                  type="submit"
+                  class="bg-orange-500 text-white rounded-[10px] px-4 py-2  md:py-3  text-base ld:text-4xl md:text-2xl"
+              >
+                Начать тест
+              </button>
+            </form>
+          </div>
+
+        </div>
+        <img
+            src="/public/img/express.svg"
+            alt="express.svg"
+            class="hidden md:block  md:w-1/2 h:w-1/2"
+        >
+      </div>
+    </section>
+
     <div
-        v-if="testStore.currentTest"
+        v-else-if="testStore.currentTest"
         class="bg-orange-500 flex flex-col gap-4 lg:gap-8 text-white lg:max-w-[90%] lg:min-h-[650px] lg:h-[70vh] mx-auto p-2 md:p-4 lg:p-8 overflow-y-auto"
     >
       <h1 class="text-lg md:text-xl lg:text-3xl">
@@ -162,10 +235,7 @@ const showResult = () => {
           {{ testStore.currentTest.result }}
         </p>
       </div>
-      <div
-          v-if="testStore.currentTest?.questions.length !== 0 && testStore.currentTest?.scales.length !== 0"
-          class="flex gap-6 mt-auto justify-end"
-      >
+      <div class="flex gap-6 mt-auto justify-end">
         <button
             @click="prevQuestion"
             class="transform rotate-90 border-2 border-white p-2 rounded-full"
